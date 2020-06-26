d65e4ed51cf3cbfb0911f75ab9dd56688fb466d9
Evert Ball <evert.ball@gmail.com>
2020-06-26 10:28:30 -0400

    Finished the basic data typesplayground

--- BasicDataTypes.playground/Contents.swift
+++ BasicDataTypes.playground/Contents.swift
@@ -3,5 +3,23 @@
 import UIKit
 import PlaygroundSupport
 
+// Constants
+let isBasicDataType: Bool = true
+let theQuestion = "What is the answer to life, the universe, and everything?"
+let apprehensiveMsg = "You're not going to like this..."
 
+// Variables
+var theAnswer = 41
+
+// This repeat-while loop obviously isn't necessary, it's just for practice
+repeat {
+    if isBasicDataType {
+        print(theQuestion)
+        theAnswer += 1
+        print("Hmmmmm...🤔")
+    }
+} while theAnswer != 42
+
+print(apprehensiveMsg)
+print("The answer to life, the universe, and everything is...\(theAnswer)")
 
