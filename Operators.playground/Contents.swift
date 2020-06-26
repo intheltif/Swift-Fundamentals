d4870a1efa41694496205049f30222dd46033d42
Evert Ball <evert.ball@gmail.com>
2020-06-26 13:20:36 -0400

    Added operators playground

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+//Extensions
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
+
+//Optionals and nil-coalescing
+var name: String?
+var person = name ?? "Mysterious Stranger"
+print("Hello, \(person)!")
+
+//String concatenation
+let begin = "This is the first half"
+let end = "this is the second half."
+
+var complete = "Concat: " + begin + " " + "and " + end
+print(complete)
+
+// String interpolation
+print("Interpolate: \(begin) and \(end)")
+
+// Basic operators
+let firstInt = 10
+let secInt = 20
+
+var sum: Int
+var product: Int
+var quotient: Double
+
+sum = firstInt + secInt
+product = firstInt * secInt
+quotient = Double(firstInt) / Double(secInt)
+
+print(sum)
+print(product)
+print(quotient)
+print(sum.isEven)
